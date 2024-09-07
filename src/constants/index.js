fc09f1a372f84c242858563b03968e1d763b131d
qwavy <nursultanr585@gmail.com>
2024-09-08 01:36:19 +0500

    change api key

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,7 @@
-
-// export const
-
-//
-// export const options = {
-//     method: 'GET',
-//     headers: {
-//         accept: 'application/json',
-//         Authorization: `Bearer ${import.meta.env.API_KEY}`
-//     }
-// }
-
 export const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjUyOTA2MjY4ZWI4NDg0ODYxNTljMjc2ZTkwNGI2MCIsIm5iZiI6MTcyNDk1NjQwNC43NDk0NDksInN1YiI6IjY2Y2Y5MTVlZTAyYWMwMzM1MTcyMmJhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h9mG-BsyQrvgK7GnoqRRlWFF0jT6gzb_LPPPghJgLUc'
+        Authorization: import.meta.env.API_KEY
     }
 };
-
-
-
