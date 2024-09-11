7c18f0541193b076d63ba4e5af91d3eb02da03e4
qwavy <nursultanr585@gmail.com>
2024-09-12 00:33:13 +0500

    layout actor page

--- src/components/actors/ActorPage.vue
+++ src/components/actors/ActorPage.vue
@@ -1,16 +1,23 @@
 <script setup>
 
 
-
-import {onMounted} from "vue";
+import {computed, onMounted} from "vue";
 import {useActorStore} from "@/stores/ActorStore.js";
 import {useRoute} from "vue-router";
 import {options} from "@/constants/index.js";
+import {getAgeFromBirthDate} from "@/lib/utils.js";
 
-const actorStore = useActorStore()
 const route = useRoute()
 
-const urlActorPage = "https://api.themoviedb.org/3/person/55638?language=en-US"
+const id = route.params.id
+
+const actorStore = useActorStore()
+
+
+
+const urlActorPage = `https://api.themoviedb.org/3/person/${id}?language=en-US`
+
+const age = computed(() => getAgeFromBirthDate(actorStore.actor.birthday))
 
 onMounted(() => {
   actorStore.getActor(urlActorPage,options)
@@ -21,16 +28,36 @@ onMounted(() => {
   <div class="actor-page">
     <div class="actor-info">
       <div class="actor-image">
-        {{actorStore.actor}}
+        <img :src="`https://image.tmdb.org/t/p/w300_and_h450_bestv2${actorStore.actor.profile_path}`"/>
       </div>
       <div class="actor-personal-info">
-        <div>
-
-        </div>
-        <h2 class="info-title">
-          Known for
+        <h2 class="actor-personal-info-title">
+          Personal Information
         </h2>
-        <p></p>
+        <div>
+          <h3 class="personal-info-title">
+            Age
+          </h3>
+          <p class="personal-info-description">
+            {{age}}
+          </p>
+        </div>
+        <div>
+          <h3 class="personal-info-title">
+            Known for
+          </h3>
+          <p class="personal-info-description">
+            {{actorStore.actor.known_for_department}}
+          </p>
+        </div>
+        <div>
+          <h3 class="personal-info-title">
+            Known for
+          </h3>
+          <p class="personal-info-description">
+            {{actorStore.actor.known_for_department}}
+          </p>
+        </div>
       </div>
     </div>
     <div class="actor-career">
@@ -40,7 +67,24 @@ onMounted(() => {
 </template>
 
 <style scoped>
+  .actor-page {
+    display: flex;
+  }
   .actor-info{
     display: flex;
+    flex-direction: column;
+    gap: 40px;
+  }
+  .actor-personal-info-title{
+    font-size: 21px;
+    font-weight: 600;
+  }
+  .personal-info-title{
+    font-size: 16px;
+    font-weight: 600;
+  }
+  .personal-info-description{
+    font-size: 16px;
+    font-weight: 400;
   }
 </style>
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function getAgeFromBirthDate(birthDateString) {
+  const today = new Date();
+  const birthDate = new Date(
+      birthDateString.substr(0, 4),
+      birthDateString.substr(4, 2) - 1,
+      birthDateString.substr(6)
+);
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  if (today.getMonth() < birthDate.getMonth() ||
+      (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age;
+}
